d03848f948e43c9c59f958adb0c1a32d3fe5a168
mariah johnson <mariahdawn9@gmail.com>
2022-10-03 05:59:33 +0000

    added onclick toggle to title

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
+//onload function and contents added form my "interactive recipe codepen https://codepen.io/mariahdawn9/pen/XWqqPMK 
+
 window.onload = function(){
 
-document.querySelector("#header h1").classList.add("makeBig"); //changes title on page load
-  ``
+document.querySelector("#header h1").classList.add("makeBig"); //changes title on page  ``
+  
+document.querySelector("#header h1").onclick = function() {
+  this.classList.toggle("makeWhite");
+  } // toggles title color on page
 
 }
 //end of window on loadp
